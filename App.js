edf5e783c7a6e43b84a520a66f3a470e46c1c204
Magno Carvalho <magnocarv@hotmail.com>
2020-12-29 21:21:20 -0300

    fix and send chane

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes/start.router";
 import Home from "./src/pages/home";
-
+import Constants from "expo-constants";
 export default function App() {
   return (
-    <SafeAreaProvider style={styles.container}>
-      <NavigationContainer>
-        <Home />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <Home />
+    </SafeAreaView>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+
 import { WebView } from "react-native-webview";
 
 const Home = () => {
   const urlLoad = "http://wmgas.com.br/delivery/silvagas#gas";
+  const Loading = () => {
+    return <ActivityIndicator></ActivityIndicator>;
+  };
   return (
     <WebView
       userAgent="Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3714.0 Mobile Safari/537.36"
       source={{ uri: urlLoad }}
       javaScriptEnabled
+      startInLoadingState={true}
+      renderLoading={() => <Loading />}
     />
   );
 };
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#34af23",
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+});
